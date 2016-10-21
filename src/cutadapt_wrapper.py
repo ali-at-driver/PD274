0c7df2abac314749c690c20800d263a8e103f0b8
Ali Dalton <ali@dg-compute-res-6.sf.drivergrp.com>
2016-10-21 16:30:18 -0700

    cleanup

--- src/cutadapt_wrapper.py
+++ src/cutadapt_wrapper.py
@@ -13,24 +13,20 @@ class Capturing(list):
     def __exit__(self, *args):
         values = re.sub(r'\s{4,}', ' ', self._stringio.getvalue())
         values = re.split('(?:===).*', values)
-        new_vals = [re.split(r'\n\n|\n|:', value) for value in values]#self._stringio.getvalue()))# values.splitlines())
+        new_vals = [re.split(r'\n\n|\n|:', value) for value in values]
 
         self.extend(new_vals)
         sys.stdout = self._stdout
 
 
-def grep_report(report):#, trim_fields=None, dimer_fields=None):
-    print(report)
-    print(len(report))
+def grep_report(report, outfile):
+    # TODO: FIX HARDCODED VALUES!!!
     fields = [Reports.processed_field, Reports.r1_trim_field, Reports.r2_trim_field]
-    print(report[1].index(fields[0]))
-    #dimer_indices0 = [i for i, x in enumerate(report[2]) if x == Reports.dimer_field]
-    #print(dimer_indices)
-    #dimer_indices1 = [i for i, x in enumerate(report[3]) if x == Reports.dimer_field]
-
     dimers0 = [ report[2][field] for field in range(report[2].index(Reports.dimer_field), len(report[2]))]
     dimers1 = [ report[3][field] for field in range(report[3].index(Reports.dimer_field), len(report[3]))]
-    with open("test.tsv", "w") as fh:
+    
+    # FIXME: make dry-er and remove hardcoded values 
+    with open(outfile, "w") as fh:
         for field in fields:
             print(field)
             fh.write(report[1][report[1].index(field)].strip() + "\t" + report[1][report[1].index(field) + 1].strip() + "\n")
@@ -38,33 +34,16 @@ def grep_report(report):#, trim_fields=None, dimer_fields=None):
             fh.write(field + "\n")
         for field in dimers1:
             fh.write(field + "\n")
-"""
-  Total read pairs processed:              1,000
-  Read 1 with adapter:                      56 (5.6%)
-  Read 2 with adapter:                      56 (5.6%)
-"""
-def grep_fields(idx, report):
-    for i in xrange(idx, len(report)):
-        final_report += report[i]
-        final_report += "\n"
-    return final_report
-
-
-def write_to_tsv():
-    with open("test.tsv", "w") as fh:
-        for e in ordered:
-            fh.write("\t\t".join(map(str, e[:2])) + "\n")
 
 def datapath(path):
     return os.path.join(os.path.dirname(__file__), path)
 
 def run(inf1, inf2, out1, out2, params=None, lab_adapt_override=None, stdout_override=True):
-    #if type(params) is str:
-    #   params = params.split()
+    # FIXME: ADD OVERRIDES 
+    params = ['-a', 'AGATCGGAAGAGC', '-A', 'AGATCGGAAGAGC',
+              '-o', out1, '-p', out2,
+              inf1, inf2 ]
 
-    params = ['-a', 'AGATCGGAAGAGC', '-A', 'AGATCGGAAGAGC']
-    params += ['-o', out1, '-p', out2]
-    params += [inf1, inf2]
     if stdout_override:
         with Capturing() as output:
             cutadapt.main(params)
@@ -76,4 +55,4 @@ def run(inf1, inf2, out1, out2, params=None, lab_adapt_override=None, stdout_ove
 if __name__=='__main__':
     output = run('../data/E144-T1-D1_S15_L004_R1_001.fastq.gz', '../data/E144-T1-D1_S15_L004_R1_001.fastq.gz','out1.fastq.gz', 'out1.fastq.gz', stdout_override=True)
 
-    grep_report(output)
+    grep_report(output, "test.tsv")
